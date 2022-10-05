809812bb4a0bae7ca4b182260e406dac4ad91224
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-05 16:18:30 -0400

    confirm and cancel button

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,12 +1,13 @@
 import React from "react";
-
-function Confirm(props) {
+import Button from "components/Button"
+import "./styles.scss";
+function Confirm (props) {
     return (
 <main className="appointment__card appointment__card--confirm">
   <h1 className="text--semi-bold">Delete the appointment?</h1>
   <section className="appointment__actions">
-    <Button danger>Cancel</Button>
-    <Button danger>Confirm</Button>
+  <Button onClick= {props.onCancel} danger>Cancel</Button>
+    <Button onClick= {props.onConfirm} danger>Confirm</Button>
   </section>
 </main>
     );
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -139,7 +141,7 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => <Show interviewers={interviewers} onEdit={action("onEdit")} onDelete={action("onDelete") }/>)
-  //.add("Show", () => <Show  interviewers={interviewers} onDelete={action("onDelete")}/>);
+  .add("Confirm", () => <Confirm onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
 
 
   
