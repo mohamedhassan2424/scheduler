1ed32ae048ee90e6d1bdf6ad767979ed6714404b
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-05 16:28:35 -0400

    error component completed

--- stories/index.js
+++ stories/index.js
@@ -16,7 +16,7 @@ import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status"
-
+import Error from "components/Appointment/Error"
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -144,5 +144,6 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show interviewers={interviewers} onEdit={action("onEdit")} onDelete={action("onDelete") }/>)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
   .add("Status", ()=> <Status  message={"Deleting"}/>)
+  .add("Error", ()=> <Error  message={"Could not delete appointment."} onClose={action("onClose")}/>)
 
   
