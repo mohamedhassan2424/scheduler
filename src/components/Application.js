d4c3d46f8bfa77898dec966a2c507eb6830233d7
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-05 20:12:38 -0400

    rendering the applicaiton

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,8 +3,50 @@ import { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import DayListItem from "./DayListItem";
+import Appointment from "./Appointment";
+
+
+
 
 export default function Application(props) {
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer: {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
   const [day, setDay] = useState("Monday");
   const days = [
     {
@@ -24,6 +66,17 @@ export default function Application(props) {
     },
   ];
 
+  const appointmentsArray = Object.values(appointments).map((eachObjectAppointments) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
+      />
+    );
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -36,7 +89,7 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day} 
+            value={day}
             onChange={setDay}
           />
         </nav>
