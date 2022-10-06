632b1cff78c25f9034bc39a92b4f1da930fa4f59
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-06 15:55:11 -0400

    got all the test cases to work

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,19 @@
-function selectUserByName(state, name) {
+/*function selectUserByName(state, name) {
     const filteredNames = state.users.filter(user => user.name === name);
     return filteredNames;
-  }
+  }*/
+
+export function getAppointmentsForDay(state, day) {
+
+    const sameDay = state.days.find(element => element.name === day)
+    if (!sameDay) {
+        return [];
+    }
+
+    const appotiments = sameDay.appointments.map((id) => {
+        return state.appointments[id];
+    })
+
+    return appotiments;
+
+}
