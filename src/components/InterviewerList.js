ae7be4525e27ccbc0575ba296a99723a506cfc90
Hanad Hassan <hanadhassan@Hanads-MBP.ht.home>
2022-10-11 12:16:43 -0400

    our first test, created the folder __test__ and the appoitnment file

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
-
+import PropTypes from 'prop-types';
 
 
 function InterviewerList(props) {
@@ -27,4 +27,7 @@ function InterviewerList(props) {
   );
 }
 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 export default InterviewerList;
